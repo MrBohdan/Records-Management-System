ca608134a87e180bd222ba470b354d04ce34721d
Bohdan Skrypnyk <mrbudda2012@gmail.com>
2020-09-04 19:37:55 +0300

    add 'one to many' relationship (between record and audit table)

--- src/main/resources/db/changelog/changes/v0001.sql
+++ src/main/resources/db/changelog/changes/v0001.sql
@@ -1,38 +1,5 @@
 CREATE EXTENSION IF NOT EXISTS hstore;
 
-CREATE TABLE IF NOT EXISTS audit (
-    audit_id serial NOT NULL,
-    table_name text NOT NULL,
-    user_name text NOT NULL,
-    action_timestamp timestamp NOT NULL default current_timestamp,
-    action text NOT NULL CHECK (action IN ('insert','delete','update')),
-    old_values hstore,
-    new_values hstore,
-    updated_cols text[],
-    query text,
-    PRIMARY KEY (audit_id)
-);
-
-CREATE OR REPLACE FUNCTION if_modified_func() RETURNS TRIGGER AS $body$
-BEGIN
-    IF tg_op = 'UPDATE' THEN
-        INSERT INTO audit (table_name, user_name, action, old_values, new_values, updated_cols, query)
-        VALUES (tg_table_name::text, current_user::text, 'update', hstore(old.*), hstore(new.*),
-               akeys(hstore(new.*) - hstore(old.*)), current_query());
-        return new;
-    ELSIF tg_op = 'DELETE' THEN
-        INSERT INTO audit (table_name, user_name, action, old_values, query)
-        VALUES (tg_table_name::text, current_user::text, 'delete', hstore(old.*), current_query());
-        return old;
-    ELSIF tg_op = 'INSERT' THEN
-        INSERT INTO audit (table_name, user_name, action, new_values, query)
-        VALUES (tg_table_name::text, current_user::text, 'insert', hstore(new.*), current_query());
-        return new;
-    END IF;
-END;
-$body$
-language plpgsql;
-
 CREATE TABLE IF NOT EXISTS "record" (
     usreou bigint NOT NULL,
     shares_amount bigint NOT NULL,
@@ -43,5 +10,39 @@ CREATE TABLE IF NOT EXISTS "record" (
     total_par_value double precision NOT NULL,
     PRIMARY KEY (usreou)
 );
+
+CREATE TABLE IF NOT EXISTS audit (
+    audit_id serial NOT NULL,
+    table_name text NOT NULL,
+    user_name text NOT NULL,
+    action_timestamp timestamp NOT NULL default current_timestamp,
+    action text NOT NULL CHECK (action IN ('insert','delete','update')),
+    usreou bigint NOT NULL REFERENCES record(usreou),
+    old_values hstore,
+    new_values hstore,
+    updated_cols text[],
+    query text,
+    PRIMARY KEY (audit_id)
+);
+
+CREATE OR REPLACE FUNCTION if_modified_func() RETURNS TRIGGER AS $body$
+BEGIN
+    IF tg_op = 'UPDATE' THEN
+        INSERT INTO audit (table_name, user_name, action, usreou, old_values, new_values, updated_cols, query)
+        VALUES (tg_table_name::text, current_user::text, 'update', new.usreou, hstore(old.*), hstore(new.*),
+               akeys(hstore(new.*) - hstore(old.*)), current_query());
+        return new;
+    ELSIF tg_op = 'DELETE' THEN
+        INSERT INTO audit (table_name, user_name, action, usreou, old_values, query)
+        VALUES (tg_table_name::text, current_user::text, 'delete', old.usreou, hstore(old.*), current_query());
+        return old;
+    ELSIF tg_op = 'INSERT' THEN
+        INSERT INTO audit (table_name, user_name, action, usreou, new_values, query)
+        VALUES (tg_table_name::text, current_user::text, 'insert', new.usreou, hstore(new.*), current_query());
+        return new;
+    END IF;
+END;
+$body$
+language plpgsql;
 
 CREATE TRIGGER record_audit AFTER INSERT OR UPDATE OR DELETE ON record FOR EACH ROW EXECUTE PROCEDURE if_modified_func();
